916a2e7901c12c187f6cd565d20b008a01e5dc65
Bogdorog <sergeevdaniil866@gmail.com>
2026-04-21 19:40:54 +0400

    Создание проекта

---
+++ src/test/java/com/sergeev/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergeev.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
